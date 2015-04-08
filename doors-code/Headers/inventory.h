c50c1429f0fa6b380e4ddf414169125ad451c231
Nick Gaulke <incomingstick@users.noreply.github.com>
2015-04-07 20:25:26 -0500

    Update inventory.h

--- doors-code/Headers/inventory.h
+++ doors-code/Headers/inventory.h
@@ -2,6 +2,11 @@
 #define INVENTORY_H
 
 #include <QDialog>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+#include <QLabel>
+#include <QList>
+#include <player.h>
 
 namespace Ui {
 class Inventory;
@@ -15,11 +20,18 @@ public:
     explicit Inventory(QWidget *parent = 0);
     ~Inventory();
 
+    void setPlayer(Player player);
+
 private slots:
     void on_pushButton_clicked();
 
 private:
     Ui::Inventory *ui;
+
+    Player player;
+
+    QList<QLabel*> inv;
+    QList<QLabel*> equiped;
 };
 
 #endif // INVENTORY_H

dc91f265633efab99060a9219e0ef280f2d498a6
Nick Gaulke <incomingstick@users.noreply.github.com>
2015-04-07 20:24:50 -0500

    Update inventory.cpp

--- doors-code/Sources/inventory.cpp
+++ doors-code/Sources/inventory.cpp
@@ -2,6 +2,7 @@
 #include "ui_inventory.h"
 #include "qdesktopwidget.h"
 
+
 Inventory::Inventory(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Inventory)
@@ -10,6 +11,19 @@ Inventory::Inventory(QWidget *parent) :
 
     this->move(QApplication::desktop()->availableGeometry().center() - this->rect().center());
     this->setWindowTitle("Inventory");
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            inv << new QLabel(this);
+            inv.at(j+i*5)->setGeometry(350 - (j * 40), 50 + (i * 40), 30, 30);
+            QPixmap axe("");//":/doors/weapons/Doors Inv/weapons/epic sword.png");
+            inv.at(j+i*5)->setPixmap(axe);
+            inv.at(j+i*5)->setScaledContents(true);
+            inv.at(j+i*5)->setStyleSheet("border: 1px solid black");
+        }
+    }
+    ui->head->setStyleSheet("border: 1px solid black");
+    ui->armor->setStyleSheet("border: 1px solid black");
+    ui->weapon->setStyleSheet("border: 1px solid black");
 }
 
 Inventory::~Inventory()
@@ -20,4 +34,8 @@ Inventory::~Inventory()
 void Inventory::on_pushButton_clicked()
 {
     this->close();
+}
+
+void Inventory::setPlayer(Player player) {
+    this->player = player;
 }
